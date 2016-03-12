810135daeddeb413f9cbac5f0f320ac32d4cc1d7
SirForez <orrigudlaugur@gmail.com>
2016-03-12 06:45:31 +0000

    Fixed stupid dumb human error in the php code which included html code which did not work correctly durr

--- hppRequestProducer.php
+++ hppRequestProducer.php
@@ -1,26 +1,17 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1 //EN"
-"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns = "http://www.w3.org/1999/xhtml">
-	<head>
-		<title>Hello World in PHP</title>
-	</head>
-	<body>
-		<?php
-			require_once ('vendor/autoload.php');
+<?php
+	require_once ('vendor/autoload.php');
 			 
-			use com\realexpayments\hpp\sdk\domain\HppRequest;
-			use com\realexpayments\hpp\sdk\RealexHpp;
+	use com\realexpayments\hpp\sdk\domain\HppRequest;
+	use com\realexpayments\hpp\sdk\RealexHpp;
 			 
-			$hppRequest = ( new HppRequest() )
-			    ->addMerchantId( "realexsandbox" )
-			    ->addAccount( "internet" )
-			    ->addAmount( "1001" )
-			    ->addCurrency( "EUR" )
-			    ->addAutoSettleFlag( "1" );
+	$hppRequest = ( new HppRequest() )
+		->addMerchantId( "realexsandbox" )
+		->addAccount( "internet" )
+		->addAmount( "1001" )
+		->addCurrency( "EUR" )
+		->addAutoSettleFlag( "1" );
 			 
-			$realexHpp = new RealexHpp( "secret" );
+	$realexHpp = new RealexHpp( "secret" );
 			 
-			$requestJson = $realexHpp->requestToJson( $hppRequest );
-		?>
-	</body>
-</html>
+	$requestJson = $realexHpp->requestToJson( $hppRequest );
+?>
--- hppResponseConsumer.php
+++ hppResponseConsumer.php
@@ -1,23 +1,14 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1 //EN"
-"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns = "http://www.w3.org/1999/xhtml">
-	<head>
-		<title>Hello World in PHP</title>
-	</head>
-	<body>
-		<?php
-			require_once ('vendor/autoload.php');
+<?php
+	require_once ('vendor/autoload.php');
  
-			use com\realexpayments\hpp\sdk\domain\HppResponse;
-			use com\realexpayments\hpp\sdk\RealexHpp;
+	use com\realexpayments\hpp\sdk\domain\HppResponse;
+	use com\realexpayments\hpp\sdk\RealexHpp;
 			 
-			$realexHpp = new RealexHpp("secret");
+	$realexHpp = new RealexHpp("secret");
 			 
-			$hppResponse = $realexHpp->responseFromJson(responseJson); // responseJson will be the PHP variable containing the JSON response string
+	$hppResponse = $realexHpp->responseFromJson(responseJson); // responseJson will be the PHP variable containing the JSON response string
 
-			$result = $hppResponse->getResult();
-			$message = $hppResponse->getMessage();
-			$authCode = $hppResponse->getAuthCode();
-		?>
-	</body>
-</html>
+	$result = $hppResponse->getResult();
+	$message = $hppResponse->getMessage();
+	$authCode = $hppResponse->getAuthCode();
+?>
