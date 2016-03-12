f54e159a89ffc69af9c9329f4dfa433c9709b5e0
SirForez <orrigudlaugur@gmail.com>
2016-03-12 06:51:59 +0000

    r

--- hppResponseConsumer.php
+++ hppResponseConsumer.php
@@ -3,12 +3,16 @@
  
 	use com\realexpayments\hpp\sdk\domain\HppResponse;
 	use com\realexpayments\hpp\sdk\RealexHpp;
-			 
+
 	$realexHpp = new RealexHpp("secret");
-			 
-	$hppResponse = $realexHpp->responseFromJson(responseJson); // responseJson will be the PHP variable containing the JSON response string
+
+	$responseJson = $_POST['hppResponse'];
+
+	$hppResponse = $realexHpp->responseFromJson($responseJson); // responseJson will be the PHP variable containing the JSON response string
 
 	$result = $hppResponse->getResult();
 	$message = $hppResponse->getMessage();
 	$authCode = $hppResponse->getAuthCode();
+
+	print_r($responseJson); 
 ?>
