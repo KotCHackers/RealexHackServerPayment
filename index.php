765c4abc5c392d6aceb4707c25f05e98d4280e02
SirForez <orrigudlaugur@gmail.com>
2016-03-12 05:28:59 +0000

    All of the things are now things. And these things may hopefully work, i decided to test it out with the example stuff on github but it gives and invalid request because it is invalid data, so ti is the expected response at the very least, or so methinks. But things are things that are doing things and this may or may not, but probably may, be a good thing.

--- index.php
+++ index.php
@@ -6,7 +6,27 @@
 	</head>
 	<body>
 		<?php
-			echo '<p>Hello World</p>'; //A one-line PHP script
+
+			require_once('vendor/autoload.php');
+			
+			use com\realexpayments\hpp\sdk\domain\HppRequest;
+			use com\realexpayments\hpp\sdk\RealexHpp;
+
+			$hppRequest = ( new HppRequest() )
+			    ->addMerchantId( "myMerchantId" )
+			    ->addAccount( "mySubAccount" )
+			    ->addAmount( "1001" )
+			    ->addCurrency( "EUR" )
+			    ->addAutoSettleFlag( "1" );
+
+			$supplementaryData = array();
+			$supplementaryData['key1'] = 'value1';
+			$supplementaryData['key2'] = 'value2';
+
+			$hppRequest->addSupplementaryData( $supplementaryData );    
+
+			$realexHpp = new RealexHpp( "mySecret" );
+			$requestJson = $realexHpp->requestToJson( $hppRequest );
 		?>
 	</body>
 </html>
